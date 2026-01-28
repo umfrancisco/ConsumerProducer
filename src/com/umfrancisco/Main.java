5aecac458c38f954fbb4bc627d9a854685d5dc0f
umfrancisco <francis.guitler@gmail.com>
2026-01-28 09:36:13 -0300

    added wait and notifyAll to prevent deadlocks

--- src/com/umfrancisco/Main.java
+++ src/com/umfrancisco/Main.java
@@ -8,17 +8,27 @@ class MessageRepository {
 	
 	public synchronized String read() {
 		while (!hasMessage) {
-			
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
 		}
 		hasMessage = false;
+		notifyAll();
 		return message;
 	}
 	
 	public synchronized void write(String message) {
 		while (hasMessage) {
-			
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
 		}
 		hasMessage = true;
+		notifyAll();
 		this.message = message;
 	}
 }
